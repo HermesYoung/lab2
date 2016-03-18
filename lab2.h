da068f68e8b95ad03fe3a66a5d975f3ffa1fcc09
HermesYoung <tails777tw@gmail.com>
2016-03-18 08:52:57 +0800

    add lab2.h

---
+++ lab2.h
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+class Data{
+	private:
+		float mass,height;
+	public:
+		float Get_Mass(float);
+		float Get_height(float);
+		float Cal_BMI();
+char const * Set_cate();
+};
